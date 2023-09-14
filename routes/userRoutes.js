d3f94b2548a8b3a5b873775b6d378979fa3c63a4
Smaranya Vijaya Krishna <smaranya.vijayakrishna@gmail.com>
2023-09-14 14:26:15 +0530

    Revert "Backend Files"
    This reverts commit 161d00532dc585df00bb7a97ad654ec8961e4c6a.

--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controller/userController');
-
-router.post('/create', userController.createUser);
-
-module.exports = router;
