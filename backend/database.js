7fb5dccf0a51325b92549bdb9a9294e2a4fdc702
Smaranya Vijaya Krishna <smaranya.vijayakrishna@gmail.com>
2023-09-15 19:47:34 +0530

    The table creation queries

--- backend/database.js
+++ backend/database.js
@@ -7,7 +7,6 @@ dotenv.config();
 const pool = new Pool({
     
     user: process.env.DB_USER,
-    host: process.env.DB_HOST || 'localhost',
     database: process.env.DB_NAME,
     password: String(process.env.DB_PASSWORD),
     port: process.env.DB_PORT || 5432,
